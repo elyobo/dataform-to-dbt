e073afb9928f789d0e12eb1fd1dda47ee503487b
Liam O'Boyle <liam@elyobo.net>
2022-10-22 22:10:48 +1100

    Extend tests, fix bq partition parsing bug
    Integer range partitions were not being parsed correctly.

--- src/dataform.js
+++ src/dataform.js
@@ -1,6 +1,7 @@
 // Parse a dataform partition by clause to get a dbt version
 export const parsePartitionBy = (value) => {
   if (!value) return undefined
+  // e.g. TIMESTAMP_TRUNC(<date_column>, MONTH)
   const trunc = value.match(
     /^(date|datetime|timestamp)_trunc\(([^,]+),\s*([^)]+)\)$/i,
   )
@@ -13,12 +14,13 @@ export const parsePartitionBy = (value) => {
     }
   }
 
+  // e.g. DATE(<date_column>)
   const date = value.match(/^date\(([^)]+)\)$/i)
   if (date) return { field: date[1], data_type: 'date', granularity: 'day' }
 
-  // RANGE_BUCKET(<integer_column>, GENERATE_ARRAY(0, 1000000, 1000))"
+  // e.g. RANGE_BUCKET(<integer_column>, GENERATE_ARRAY(0, 1000000, 1000))
   const int = value.match(
-    /^range_bucket\(([^,]+),\s*generate_array\((\d+),\s*(\d+),\s*(\d+)$/i,
+    /^range_bucket\(([^,]+),\s*generate_array\((\d+),\s*(\d+),\s*(\d+)\)\)$/i,
   )
   if (int) {
     const [, field] = int
--- src/dataform.test.js
+++ src/dataform.test.js
@@ -6,7 +6,27 @@ describe('parsePartitionBy()', () => {
       'DATE(some_field)',
       { field: 'some_field', data_type: 'date', granularity: 'day' },
     ],
-  ])('"%p" is parsed as %p', (input, output) => {
+    [
+      'DATETIME_TRUNC(some_field, HOUR)',
+      { field: 'some_field', data_type: 'datetime', granularity: 'hour' },
+    ],
+    [
+      'TIMESTAMP_TRUNC(some_field, HOUR)',
+      { field: 'some_field', data_type: 'timestamp', granularity: 'hour' },
+    ],
+    [
+      'TIMESTAMP_TRUNC(some_field, MONTH)',
+      { field: 'some_field', data_type: 'timestamp', granularity: 'month' },
+    ],
+    [
+      'RANGE_BUCKET(some_field, GENERATE_ARRAY(0, 1000000, 1000))',
+      {
+        field: 'some_field',
+        data_type: 'int64',
+        range: { start: 0, end: 1000000, interval: 1000 },
+      },
+    ],
+  ])('%p is parsed as %p', (input, output) => {
     expect.hasAssertions()
     expect(parsePartitionBy(input)).toStrictEqual(output)
   })
